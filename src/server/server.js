989cf4bed3c71234f9797826116443fea4fc044e
kanedev <kanedev@live.fr>
2019-10-22 18:42:20 +0200

    ✨Feat : Send data with method POST

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const bodyPar= require('body-parser');
+let urlEncoded= bodyPar.urlencoded({extended:false});
+
 
 const app = express(),
             DIST_DIR = path.join(__dirname, '../../public'),
@@ -9,27 +12,27 @@ app.use(express.static(DIST_DIR))
 
 
 
-const mockResponse = {
-    foo: 'bar',
-    bar: 'foo'
-  };
+// const mockResponse = {
+//     foo: 'bar',
+//     bar: 'foo'
+//   };
 
-  app.get('/api', (req, res) => {
+  app.post('/api',urlEncoded, (req, res) => {
    // res.send(mockResponse);
 
 let response={
-  firstName : req.query.nom ,
-  lastName : req.query.prenom
+  firstName : req.body.nom ,
+  lastName : req.body.prenom
 }
 
-res.send('<p>' + req.query.nom +' ' + req.query.prenom + '</p>')
-res.send(JSON.stringify(response));
+res.send('<p>' + req.body.nom +' ' + req.body.prenom + '</p>')
+//res.send(JSON.stringify(response)); 
 
   });
 
 
   app.get('/', (req, res) => {
-   // res.sendFile(HTML_FILE); // EDIT
+   res.sendFile(HTML_FILE); // EDIT
     // res.redirect('http://localhost:5000/');
   });
 
